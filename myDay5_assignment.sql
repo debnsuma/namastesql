6f714967d4b26405f27b275e6fd88f30039071e5
Suman Debnath <56056673+debnsuma@users.noreply.github.com>
2022-12-10 15:28:30 +0000

    adding day5 assignment

--- myDay5_assignment.sql
+++ myDay5_assignment.sql
@@ -62,8 +62,66 @@ SELECT * FROM employee;
 
 SELECT * FROM dept;
 
-SELECT COUNT(DISTINCT e.salary) as distinct_salary, COUNT(*) as no_employee
+SELECT d.dep_name, COUNT(DISTINCT e.salary) as distinct_salary, COUNT(*) as no_employee
 FROM employee e 
 INNER JOIN dept d ON e.dept_id = d.dep_id
-GROUP BY d.dep_id
+GROUP BY d.dep_name
 HAVING COUNT(DISTINCT e.salary) = COUNT(*)
+
+-- Q5. write a query to print sub categories where we have all 3 kinds of returns (others,bad quality,wrong items)
+
+SELECT TOP 2 * FROM orders;
+
+SELECT TOP 2 * FROM returns;
+
+SELECT o.Sub_Category, COUNT(*) as count
+FROM orders o 
+INNER JOIN returns r ON o.Order_ID = r.[Order Id]
+GROUP BY o.Sub_Category;
+
+-- Q6. write a query to find cities where not even a single order was returned.
+
+SELECT TOP 2 * FROM orders;
+
+SELECT TOP 2 * FROM returns;
+
+SELECT o.State, COUNT(*) as count
+FROM orders o 
+LEFT JOIN returns r ON o.Order_ID = r.[Order Id]
+WHERE r.[Return Reason] IS NULL 
+GROUP BY o.State
+
+-- Q7. write a query to find top 3 subcategories by sales of returned orders in east region
+
+SELECT TOP 2 * FROM orders;
+
+SELECT TOP 2 * FROM returns;
+
+SELECT TOP 3 o.Sub_Category, SUM(o.sales) as total_sales
+FROM orders o 
+INNER JOIN returns r ON o.Order_ID = r.[Order Id]
+GROUP BY o.Sub_Category
+ORDER BY total_sales DESC
+
+-- Q8. write a query to print dep name for which there is no employee
+
+SELECT * FROM employee;
+
+SELECT * FROM dept;
+
+SELECT d.dep_name, COUNT(*) as emp_count
+FROM dept d
+LEFT JOIN employee e ON d.dep_id = e.dept_id
+WHERE e.emp_id is NULL
+GROUP BY d.dep_name
+
+-- Q9. write a query to print employees name for dep id is not avaiable in dept table
+
+SELECT * FROM employee;
+
+SELECT * FROM dept;
+
+SELECT e.emp_name
+FROM employee e
+LEFT JOIN dept d ON d.dep_id = e.dept_id
+WHERE d.dep_id IS NULL
